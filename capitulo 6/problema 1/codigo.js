03012b37ad0d1df373713876774dc6a35c9eb16d
sergioruizdeveloper <sergioruizdeveloper@gmail.com>
2021-01-16 19:36:04 -0300

    no funciona el ...

--- capitulo 6/problema 1/codigo.js
+++ capitulo 6/problema 1/codigo.js
@@ -11,7 +11,7 @@ function crearLlave(nombre,modelo,precio){
 
 let documentFragment = document.createDocumentFragment();
 
-for (var i = 1; i <= 20; i++) {
+for (var i = 1; i <= 21; i++) {
 	let modeloRandom = Math.round(Math.random()*10000);
 	let precioRandom = Math.round(Math.random()*10+30);
 	let llave = crearLlave(`llave ${i}`,`modelo ${modeloRandom}`,precioRandom);
@@ -21,7 +21,8 @@ for (var i = 1; i <= 20; i++) {
     });
     div.tabIndex = i;
     div.classList.add(`item-${i}`,'flex-item');
-    div.innerHTML = ...llave;
+	div.innerHTML = llave[0]+llave[1]+llave[2]+llave[3];
+	// div.innerHTML=...llave;
 	documentFragment.appendChild(div);
 }
 
